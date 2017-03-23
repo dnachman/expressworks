b86ef6700b5abd3ab35e3e713fd0b32de222b368
David Nachman <david@nachman.org>
2017-03-22 21:58:14 -0400

    lesson 2 - static content

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let express = require('express');
+let path = require('path');
 
 var app = express();
 app.get('/home', function(req, res) {
     res.end('Hello World!');
 });
+
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
 
 app.listen(process.argv[2]);
