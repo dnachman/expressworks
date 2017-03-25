1cd7d0bcabd36881013a14f0d218eac847998a81
David Nachman <david@nachman.org>
2017-03-24 22:00:18 -0400

    end of 4 - forms

---
+++ index-4.js
@@ -0,0 +1,28 @@
+let express = require('express');
+let path = require('path');
+let bodyparser = require('body-parser');
+
+var app = express();
+
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.set('views', process.argv[3] || path.join(__dirname, 'templates'));
+
+app.set('view engine', 'pug');
+
+app.get('/home', function(req, res) {
+    res.render('index', {date: new Date().toDateString()});
+});
+
+app.get('/form', function(req, res) {
+    //res.end('<form><input name="str"/></form>');
+    res.render('form', {});
+});
+
+app.post('/form', function(req, res) {
+    console.log('post called');
+    res.end(req.body.str.split('').reverse().join(''));
+});
+
+app.listen(process.argv[2]);
