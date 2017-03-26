9c2512116fb77a28d0b69f243696269fcbde14f2
David Nachman <david@nachman.org>
2017-03-25 21:21:42 -0400

    lesson 6 - request params

---
+++ index-6.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let path = require('path');
+let bodyparser = require('body-parser');
+
+var app = express();
+
+app.put('/message/:id', function(req, res) {
+    res.end(require('crypto')
+      .createHash('sha1')
+      .update(new Date().toDateString() + req.params.id)
+      .digest('hex'));
+});
+
+app.listen(process.argv[2]);
